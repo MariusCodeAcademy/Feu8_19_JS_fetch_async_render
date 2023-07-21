ab9e37dfb43fff86030573d1d345f6bad9ed8bfe
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-21 11:48:55 +0300

    cards start

--- js/fetch.js
+++ js/fetch.js
@@ -6,14 +6,14 @@ console.log('fetch.js file was loaded');
 fetch('data.txt')
   .then((response) => {
     // blue = 'green';
-    console.log('response ===', response);
+    // console.log('response ===', response);
     if (response.ok === false) {
       throw new Error(`status: ${response.status} - ${response.statusText} `);
     }
     return response.text();
   })
   .then((dataInJs) => {
-    console.log('dataInJs ===', dataInJs);
+    // console.log('dataInJs ===', dataInJs);
   })
   .catch((errorObj) => {
     console.warn(errorObj);
@@ -24,11 +24,11 @@ fetch('data.txt')
 const url = 'https://jsonplaceholder.typicode.com/posts';
 fetch(url)
   .then((response) => {
-    console.log('response posts ===', response);
+    // console.log('response posts ===', response);
     return response.json();
   })
   .then((postArrInJs) => {
-    console.log(postArrInJs.slice(0, 10));
+    // console.log(postArrInJs.slice(0, 10));
   })
   .catch((error) => {
     console.warn(error);
@@ -36,3 +36,28 @@ fetch(url)
 
 // parsiusti ir atspausdinti userius is https://jsonplaceholder.typicode.com/users
 // atspausdinti pirmo userio name htmle
+
+const urlUsers = 'https://jsonplaceholder.typicode.com/users';
+
+let usenName = 'not set';
+fetch(urlUsers)
+  .then((response) => response.json())
+  .then((usersInJs) => {
+    // cia mes jau gavo userius
+    console.log('usersInJs ===', usersInJs);
+    // pirmo userio vardas
+    console.log('usersInJs[0].name ===', usersInJs[0].name);
+    usenName = usersInJs[0].name;
+    printValueToHtml(usenName);
+  })
+  .catch((error) => {
+    console.warn(error);
+  });
+
+function printValueToHtml(valueString) {
+  const pEl = document.createElement('p');
+  pEl.textContent = valueString;
+  document.body.append(pEl);
+}
+printValueToHtml('Ar veikia spausdinimas');
+printValueToHtml(usenName);
